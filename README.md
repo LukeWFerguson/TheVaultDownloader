4ef4914ec949a1034e47a5065db130f459b1a79d
LukeWFerguson <lukewferguson@gmail.com>
2020-07-16 11:02:10 -0500

    First pass.

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # TheVaultDownloader
 
 Downloads the whole of The Vault for GameCube ROMs.
+
+## Requirements
+
+* Python 3.7
+* Google Chrome
+* [Chrome Selenium Driver](https://selenium-python.readthedocs.io/installation.html#drivers)
+    * Also add to your `PATH`.
+* Setup for Windows use.
+
+## Executing Script
+
+```code
+python app.py
+```
--- app.py
+++ app.py
@@ -1,1 +1,69 @@
-print('Hello world.')
+import os
+import time
+import requests
+
+from bs4 import BeautifulSoup
+from pathlib import Path
+from selenium.webdriver import Chrome
+from string import ascii_uppercase
+
+BASE_URL = 'https://vimm.net'
+CONSOLE_URL = BASE_URL + '/vault/GameCube/'
+DOWNLOAD_FOLDER = os.environ['USERPROFILE'] + '/Downloads'
+
+
+def is_download_finished(temp_folder):
+    firefox_temp_file = sorted(Path(temp_folder).glob('*.part'))
+    chrome_temp_file = sorted(Path(temp_folder).glob('*.crdownload'))
+    downloaded_files = sorted(Path(temp_folder).glob('*.*'))
+
+    if (len(firefox_temp_file) == 0) and (len(chrome_temp_file) == 0) and (len(downloaded_files) >= 1):
+        return True
+    else:
+        return False
+
+
+def download_files(url):
+    try:
+        print('Downloading games from: ' + url)
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        table = soup.find('table', class_='rounded centered cellpadding1 hovertable')
+        rows = table.find_all('tr')
+
+        for row in rows:
+            print('\tDownloading: ' + str(row.find('a').string))
+            game_url = BASE_URL + row.find('a')['href']
+            driver = Chrome(executable_path='resources/chromedriver_win32/chromedriver.exe')
+            driver.get(game_url)
+    
+            # Click on `Download` button.
+            driver.find_element_by_name("download").click()
+    
+            # Wait for download to start.
+            time.sleep(10)
+    
+            # Wait for download to finish.
+            while True:
+                if is_download_finished(DOWNLOAD_FOLDER):
+                    break
+                else:
+                    time.sleep(10)
+    
+            driver.close()
+    except AttributeError:
+        print("No games found here: " + url)
+
+
+def main():
+    # Have to do the number one separately.
+    number_url = BASE_URL + '/vault/?p=list&system=GameCube&section=number'
+    download_files(number_url)
+
+    # Iterate through each letter of the alphabet.
+    for letter in ascii_uppercase:
+        download_files(CONSOLE_URL + letter)
+
+
+if __name__ == "__main__":
+    main()

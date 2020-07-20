6a987fd247233a1e971c89fcf1dd80fcee754942
LukeWFerguson <lukewferguson@gmail.com>
2020-07-19 20:57:30 -0500

    Adding ability to pass in list of games.

--- app.py
+++ app.py
@@ -8,8 +8,10 @@ from selenium.webdriver import Chrome
 from string import ascii_uppercase
 
 BASE_URL = 'https://vimm.net'
+NUMBER_URL = BASE_URL + '/vault/?p=list&system=GameCube&section=number'
 CONSOLE_URL = BASE_URL + '/vault/GameCube/'
 DOWNLOAD_FOLDER = os.environ['USERPROFILE'] + '/Downloads'
+GAMES_TO_DOWNLOAD = ['Amazing Island', 'Aggressive Inline', 'Zoids: Battle Legends']  # If you want to download everything, make this `None`.
 
 
 def is_download_finished(temp_folder):
@@ -32,33 +34,35 @@ def download_files(url):
         rows = table.find_all('tr')
 
         for row in rows:
-            print('\tDownloading: ' + str(row.find('a').string))
-            game_url = BASE_URL + row.find('a')['href']
-            driver = Chrome(executable_path='resources/chromedriver_win32/chromedriver.exe')
-            driver.get(game_url)
-    
-            # Click on `Download` button.
-            driver.find_element_by_name("download").click()
-    
-            # Wait for download to start.
-            time.sleep(10)
-    
-            # Wait for download to finish.
-            while True:
-                if is_download_finished(DOWNLOAD_FOLDER):
-                    break
-                else:
-                    time.sleep(10)
-    
-            driver.close()
+            game_name = str(row.find('a').string)
+
+            if GAMES_TO_DOWNLOAD is None or any(x in game_name for x in GAMES_TO_DOWNLOAD):
+                print('\tDownloading: ' + game_name)
+                game_url = BASE_URL + row.find('a')['href']
+                driver = Chrome(executable_path='resources/chromedriver_win32/chromedriver.exe')
+                driver.get(game_url)
+
+                # Click on `Download` button.
+                driver.find_element_by_name("download").click()
+
+                # Wait for download to start.
+                time.sleep(10)
+
+                # Wait for download to finish.
+                while True:
+                    if is_download_finished(DOWNLOAD_FOLDER):
+                        break
+                    else:
+                        time.sleep(10)
+
+                driver.close()
     except AttributeError:
         print("No games found here: " + url)
 
 
 def main():
     # Have to do the number one separately.
-    number_url = BASE_URL + '/vault/?p=list&system=GameCube&section=number'
-    download_files(number_url)
+    download_files(NUMBER_URL)
 
     # Iterate through each letter of the alphabet.
     for letter in ascii_uppercase:
